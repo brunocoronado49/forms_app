aa59051acf8ea2fc596dcbf8ac9b46c69774c403
Francisco Rangel <francorangelcoronado@gmail.com>
2024-10-22 20:30:31 -0600

    terminando el formulario tradicional

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -37,20 +37,69 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
-          CustomTextFormField(),
+          CustomTextFormField(
+            label: 'Username',
+            onChanged: (value) => username = value,
+            validator: (value) {
+              if(value == null || value.isEmpty) return 'Campo requerido';
+              if(value.trim().isEmpty) return 'Campo requerido';
+              if(value.length < 6) return 'Username requiere mas de 6 letras';
+              return null;
+            },
+          ),
           const SizedBox(height: 10),
-          CustomTextFormField(),
+          CustomTextFormField(
+            label: 'Email',
+            onChanged: (value) => email = value,
+            validator: (value) {
+              if(value == null || value.isEmpty) return 'Campo requerido';
+              if(value.trim().isEmpty) return 'Campo requerido';
+              final emailRegExp = RegExp(
+                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+              );
+
+              if(!emailRegExp.hasMatch(value)) return 'No tiene formato de correo';
+              return null;
+            },
+          ),
+          const SizedBox(height: 10),
+          CustomTextFormField(
+            label: 'Password',
+            obscure: true,
+            onChanged: (value) => password = value,
+            validator: (value) {
+              if(value == null || value.isEmpty) return 'Campo requerido';
+              if(value.trim().isEmpty) return 'Campo requerido';
+              if(value.length < 6) return 'Password requiere mas de 6 letras';
+              return null;
+            },
+          ),
           const SizedBox(height: 20),
           FilledButton.tonalIcon(
-            onPressed: () {},
+            onPressed: () {
+              final isValid = _formKey.currentState!.validate();
+              if(!isValid) return;
+              print('$username, $email, $password');
+            },
             icon: const Icon(Icons.save),
             label: const Text('Guardar usuario'),
           ),
--- lib/presentation/widgets/inputs/custom_text_form_field.dart
+++ lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final bool obscure;
+
+  const CustomTextFormField({
+    super.key,
+    this.label,
+    this.hint,
+    this.errorMessage,
+    this.onChanged,
+    this.validator,
+    this.obscure = false
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -12,17 +27,19 @@ class CustomTextFormField extends StatelessWidget {
     );
 
     return TextFormField(
-      onChanged: (value) {
-        print(value);
-      },
-      validator: (value) {
-        if(value == null) return 'Campo es requerido';
-        if(value.isEmpty) return 'Campo es requerido';
-        return null;
-      },
+      onChanged: onChanged,
+      validator: validator,
+      obscureText: obscure,
       decoration: InputDecoration(
         enabledBorder: border,
+        errorBorder: border.copyWith(borderSide: const BorderSide(color: Colors.red)),
+        focusedErrorBorder: border.copyWith(borderSide: const BorderSide(color: Colors.red)),
         focusedBorder: border.copyWith(borderSide: BorderSide(color: colors.primary)),
+        isDense: true,
+        label: label != null ? Text(label!) : null,
+        hintText: hint,
+        focusColor: colors.primary,
+        errorText: errorMessage,
       ),
     );
   }
